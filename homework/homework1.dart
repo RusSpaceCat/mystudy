deb3e71d512bad5653c6eff135f14a1335ba7fd3
RusSpaceCat <leontiev.danila@gmail.com>
2022-05-16 21:42:03 +0300

    homework 1 done

--- homework/homework1.dart
+++ homework/homework1.dart
@@ -1,3 +1,12 @@
 void main (){
-  print("Hello, World!");
+  nativenumbers(3);
+}
+
+void nativenumbers (double a){
+  if (a % 1 == 0){
+    print("Число $a является натуральным числом");
+  }
+  else {
+    print("Число $a не является натуральным числом");
+  }
 }
